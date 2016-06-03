0042bc00b84d11ac996d5bbb5e9dd3f73811eb78
Duong Quang Son <quangson91@gmail.com>
2016-06-03 09:31:09 +0700

    rename id input text

--- app/src/main/java/vnzit/com/texttospeech/MainActivity.java
+++ app/src/main/java/vnzit/com/texttospeech/MainActivity.java
@@ -1,13 +1,19 @@
 package vnzit.com.texttospeech;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText
     }
 }
