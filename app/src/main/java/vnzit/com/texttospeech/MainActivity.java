6d3e58fc0a10e42b88078dfb6fb0472a6500a397
Duong Quang Son <quangson91@gmail.com>
2016-06-03 09:45:17 +0700

    implement text to speech

--- app/src/main/java/vnzit/com/texttospeech/MainActivity.java
+++ app/src/main/java/vnzit/com/texttospeech/MainActivity.java
@@ -1,11 +1,15 @@
 package vnzit.com.texttospeech;
 
+import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     private TextToSpeech textToSpeech;
 
@@ -14,6 +18,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText
+        textToSpeech = new TextToSpeech(this, this);
+        final EditText etInput = (EditText) findViewById(R.id.etInput);
+        findViewById(R.id.btnSpeak).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autoSpeak(etInput.getText().toString());
+            }
+        });
+    }
+
+    private void autoSpeak(String text) {
+        if (TextUtils.isEmpty(text) || textToSpeech == null) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "SpeakText");
+        } else {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopTextToSpeech();
+        super.onDestroy();
+    }
+
+    private synchronized void stopTextToSpeech() {
+        if (textToSpeech == null) return;
+        textToSpeech.stop();
+        textToSpeech.shutdown();
+        textToSpeech = null;
+    }
+    @Override
+    public void onInit(int status) {
+        if (status != TextToSpeech.SUCCESS) {
+            Log.d("InitTextToSpeech", "init text to speech failed; status: " + status);
+            textToSpeech = null;
+        }
     }
 }
